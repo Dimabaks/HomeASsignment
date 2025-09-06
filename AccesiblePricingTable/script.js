b8e2fa6d3dcd4bd600a4c5732a3f15ed446a2d23
Dmitrii <dimonbacsa@gmail.com>
2025-09-06 15:27:08 +0300

    Add all files

--- AccesiblePricingTable/script.js
+++ AccesiblePricingTable/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const pricingTd = document.querySelectorAll('.price td');
+const monthlyBtn = document.querySelector('.monthly');
+const yearlyBtn = document.querySelector('.yearly');
+
+monthlyBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    pricingTd.forEach(td => td.textContent = td.dataset.monthly);
+    monthlyBtn.classList.add('active');
+    yearlyBtn.classList.remove('active');
+});
+
+yearlyBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    pricingTd.forEach(td => td.textContent = td.dataset.yearly);
+    monthlyBtn.classList.remove('active');
+    yearlyBtn.classList.add('active');
+})
